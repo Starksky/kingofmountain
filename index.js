fd671ad41c12729a89add1e10dfd0394eaabb9ca
stanislav loktev <stanislav.loktev@aleskeroff.ru>
2020-11-04 15:01:30 +0300

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.on('error', (err) => {
 
 server.on('message', (msg, rinfo) => {
   console.log('server got: ${msg} from ${rinfo.address}:${rinfo.port}');
-  server.send("привет ANDROID", rinfo.port, rinfo.address);
+  server.send(Buffer.from("привет ANDROID"), rinfo.port, rinfo.address);
 });
 
 server.on('listening', () => {
